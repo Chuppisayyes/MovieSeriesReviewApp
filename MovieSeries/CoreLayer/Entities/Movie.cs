736a76554d584785674ca8a1e920adb8e964242c
THIEN BAO <baotruong.31221020819@st.ueh.edu.vn>
2025-02-20 22:02:53 +0700

    final

---
+++ MovieSeries/CoreLayer/Entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MovieSeries.CoreLayer.Entities
+{
+    [Table("MoviesSeries")]
+    public class Movie
+    {
+        [Key]
+        [Column("movie_series_id")] // Chắc chắn SQL Server dùng tên này
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        [Column("release_date")] // Kiểm tra xem SQL Server có tên này không
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+        public ICollection<MovieSeriesTag> MovieSeriesTags { get; set; }
+    }
+}
